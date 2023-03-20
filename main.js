5188d5788d7236c51b475e4f872c8f2923fe33a0
felix-richard <105433778+felix-richard@users.noreply.github.com>
2023-03-20 17:42:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+img="";
+function setup()
+{
+    canvas=createCanvas(640,420);
+    canvas.center();
+}
+function preload(){
+    img=loadImage('dog_cat.jpg');
+}
+function draw()
+{
+    image(img,0,0,640,420);
+    fill("#FF0000");
+    text("dog",45,75);
+    noFill();
+    stroke("#FF0000")
+    rect(30,60,450,350);
+
+    
+    fill("#FF0000");
+    text('cat',320,120)
+    noFill()
+    stroke("#FF0000");
+    rect(300,90,270,320);
+
+    fill("#001d3d");
+    text('bowl',260,350)
+    noFill()
+    stroke("#001d3d");
+    rect(250,320,170,100);
+
+}
