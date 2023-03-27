0ce36bf381c96925934c55875229964b79210d06
felix-richard <105433778+felix-richard@users.noreply.github.com>
2023-03-27 17:39:43 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,25 +14,31 @@ function preload(){
 function draw()
 {
     image(img,0,0,640,420);
-    fill("#FF0000");
-    text("dog",45,75);
-    noFill();
-    stroke("#FF0000")
-    rect(30,60,450,350);
+
+    if(status != "")
+    {
+        for(i=0 ; i< objects.length;i++ )
+
+
+        {
+            document.getElementById("status").innerHTML
+ = "status : object detected";
+
+ fill("#FF0000");
+ percent = floor(objects[i].confidence * 100);
+ text(objects[i].label+''+percent+ '%',objects[i].x +20,objects[i].y +20);
+ noFill();
+ stroke("#FF0000");
+ rect(objects[i].x,objects[i].y,objects[i].width,objects[i].height);
+
+}
+
+    }
+}
 
     
-    fill("#FF0000");
-    text('cat',320,120)
-    noFill()
-    stroke("#FF0000");
-    rect(300,90,270,320);
-
-    fill("#001d3d");
-    text('bowl',260,350)
-    noFill()
-    stroke("#001d3d");
-    rect(250,320,170,100);
-}
+    
+    
     function modelLoaded()
     {
         console.log("ModelLoaded!")
@@ -45,6 +51,6 @@ function draw()
 console.log(error);
         }
 console.log(results);
-object=results;
+objects=results;
     }
 
